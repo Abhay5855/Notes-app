f8ac1b9c15543516db3e528db563fc4c9fc892fd
abhay <ap3919529@gmail.com>
2023-12-29 23:20:06 +0530

    refactor:remove the logs,move colors to the utils, remove unused var and functions

--- src/components/404/error-page.jsx
+++ src/components/404/error-page.jsx
@@ -4,7 +4,6 @@ import errorLogo from "../../assets/images/404.svg";
 import { NavLink } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error, "status");
 
   return (
     <div>
---
+++ utils/utility.jsx
@@ -0,0 +1,26 @@
+export const colors = [
+  {
+    id: 1,
+    color: "#77172e",
+  },
+  {
+    id: 2,
+    color: "#692b17",
+  },
+  {
+    id: 3,
+    color: "#7c4a03",
+  },
+  {
+    id: 4,
+    color: "#264d3b",
+  },
+  {
+    id: 5,
+    color: "#0c625d",
+  },
+  {
+    id: 6,
+    color: "#4b443a",
+  },
+];
