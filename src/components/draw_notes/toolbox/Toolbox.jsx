86b643ad55ec6f5d59fdf561d81f784a704699e4
abhay <ap3919529@gmail.com>
2024-01-04 21:44:08 +0530

    feat:implement a toolbox slice and interact with change in the color and brush size

--- src/components/draw_notes/toolbox/Toolbox.jsx
+++ src/components/draw_notes/toolbox/Toolbox.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "./toolbox.module.css";
-import { colors } from "../../../../utils/utility";
-import { useSelector } from "react-redux";
-import { MENU_ITEMS } from "../../../../utils/utility";
-
-const Toolbox = () => {
-  const selectedItem = useSelector((state) => state.menu.activeMenuItem);
-
-  const showStrokeOptions = selectedItem === MENU_ITEMS.PENCIL;
-  const showBrushOptions =
-    selectedItem === MENU_ITEMS.ERASER || selectedItem === MENU_ITEMS.PENCIL;
-
+import { COLORS } from "../../../../utils/utility";
+const Toolbox = ({
+  showBrushOptions,
+  showStrokeOptions,
+  handleBrushSize,
+  handleChangeColor,
+  size,
+}) => {
   return (
     <>
       <div className={styles.toolbox__container}>
@@ -18,12 +15,34 @@ const Toolbox = () => {
           <div className={styles.toolbox__color__contain}>
             <span>stroke color</span>
             <div className={styles.toolbox__colors}>
-              {colors.map((item) => (
-                <div
-                  key={item.id}
-                  style={{ backgroundColor: `${item.color}` }}
-                ></div>
-              ))}
+              <div
+                onClick={() => handleChangeColor(COLORS.BLACK)}
+                style={{ backgroundColor: `${COLORS.BLACK}` }}
+              ></div>
+              <div
+                onClick={() => handleChangeColor(COLORS.WHITE)}
+                style={{ backgroundColor: `${COLORS.WHITE}` }}
+              ></div>
+              <div
+                onClick={() => handleChangeColor(COLORS.GREEN)}
+                style={{ backgroundColor: `${COLORS.GREEN}` }}
+              ></div>
+              <div
+                onClick={() => handleChangeColor(COLORS.BLUE)}
+                style={{ backgroundColor: `${COLORS.BLUE}` }}
+              ></div>
+              <div
+                onClick={() => handleChangeColor(COLORS.ORANGE)}
+                style={{ backgroundColor: `${COLORS.ORANGE}` }}
+              ></div>
+              <div
+                onClick={() => handleChangeColor(COLORS.RED)}
+                style={{ backgroundColor: `${COLORS.RED}` }}
+              ></div>
+              <div
+                onClick={() => handleChangeColor(COLORS.YELLOW)}
+                style={{ backgroundColor: `${COLORS.YELLOW}` }}
+              ></div>
             </div>
           </div>
         )}
@@ -31,7 +50,13 @@ const Toolbox = () => {
         {showBrushOptions && (
           <div className={styles.toolbar__brush}>
             <span>brush size</span>
-            <input type="range" />
+            <input
+              type="range"
+              min={1}
+              max={10}
+              value={size}
+              onChange={handleBrushSize}
+            />
           </div>
         )}
       </div>
--- src/components/draw_notes/toolbox/ToolboxContainer.jsx
+++ src/components/draw_notes/toolbox/ToolboxContainer.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import Toolbox from "./Toolbox";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  changeColor,
+  changeBrushSize,
+} from "../../../redux/slice/toolboxSlice";
+import { MENU_ITEMS } from "../../../../utils/utility";
 
 const ToolboxContainer = () => {
+  const dispatch = useDispatch();
+  const selectedItem = useSelector((state) => state.menu.activeMenuItem);
+  const { size } = useSelector((state) => state.toolbox[selectedItem]);
+
+  const handleBrushSize = (e) => {
+    dispatch(changeBrushSize({ item: selectedItem, size: e.target.value }));
+  };
+
+  const handleChangeColor = (updatedColor) => {
+    dispatch(changeColor({ item: selectedItem, color: updatedColor }));
+  };
+
+  const showStrokeOptions = selectedItem === MENU_ITEMS.PENCIL;
+  const showBrushOptions =
+    selectedItem === MENU_ITEMS.ERASER || selectedItem === MENU_ITEMS.PENCIL;
+
   return (
     <div>
-      <Toolbox />
+      <Toolbox
+        showBrushOptions={showBrushOptions}
+        showStrokeOptions={showStrokeOptions}
+        handleBrushSize={handleBrushSize}
+        handleChangeColor={handleChangeColor}
+        size={size}
+      />
     </div>
   );
 };
