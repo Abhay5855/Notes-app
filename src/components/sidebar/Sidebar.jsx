917331d7d06e1c11db8cc2335bceebdaf732a6e2
abhay <ap3919529@gmail.com>
2024-01-08 17:31:43 +0530

    refactor:chnage the sidebar to sketch

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,37 +5,39 @@ import { NavLink } from "react-router-dom";
 const Sidebar = () => {
   return (
     <>
-      <div className='sidebar__container'>
+      <div className="sidebar__container">
         <ul>
           <li>
-            <NavLink to='/home'>
+            <NavLink to="/home">
               <div>
-                <span class='material-symbols-outlined'>description</span>
-                <h3 className='sidebar__labels'>Notes</h3>
+                <span class="material-symbols-outlined">description</span>
+                <h3 className="sidebar__labels">Notes</h3>
               </div>
             </NavLink>
           </li>
           <li>
-            <NavLink to='/favourite'>
+            <NavLink to="/favourite">
               <div>
-                <span class='material-symbols-outlined'>favorite</span>
-                <h3 className='sidebar__labels'>Favourite</h3>
+                <span class="material-symbols-outlined">favorite</span>
+                <h3 className="sidebar__labels">Favourite</h3>
               </div>
             </NavLink>
           </li>
           <li>
-            <NavLink to='/thrash'>
+            <NavLink to="/thrash">
               <div>
-                <span class='material-symbols-outlined'>delete</span>
-                <h3 className='sidebar__labels'>Thrash</h3>
+                <span class="material-symbols-outlined">
+                  <span class="material-symbols-outlined">draw</span>
+                </span>
+                <h3 className="sidebar__labels">Sketch</h3>
               </div>
             </NavLink>
           </li>
           <li>
-            <NavLink to='/tags'>
+            <NavLink to="/tags">
               <div>
-                <span class='material-symbols-outlined'>sell</span>
-                <h3 className='sidebar__labels'>Tags</h3>
+                <span class="material-symbols-outlined">sell</span>
+                <h3 className="sidebar__labels">Tags</h3>
               </div>
             </NavLink>
           </li>
