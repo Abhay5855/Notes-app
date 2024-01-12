747313649c9eaf97ba91ccd21d23b525d47033c5
abhay <ap3919529@gmail.com>
2024-01-12 09:56:34 +0530

    fix:config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   proxy: {
     "/api": {
       target: process.env.VITE_BASE_URL,
