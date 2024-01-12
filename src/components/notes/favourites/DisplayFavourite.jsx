08b9fe6f72ac129c5f0fd2e1b24045bae981926a
abhay <ap3919529@gmail.com>
2024-01-13 00:32:40 +0530

    fix:show the drawn imag in the favourit section as well

--- src/components/notes/favourites/DisplayFavourite.jsx
+++ src/components/notes/favourites/DisplayFavourite.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useRef } from "react";
 import "./favourite.css";
 import Bricks from "bricks.js";
 import Loader from "../../../base/loader/Loader";
+import { useSelector } from "react-redux";
 
 const DisplayFavourite = ({ notes, isLoading }) => {
   const mainContainerRef = useRef(null);
   const noteElRefs = useRef([]);
+  const noteImages = useSelector((state) => state.note.noteImages);
 
   useEffect(() => {
     const sizes = [
       { columns: 1, gutter: 10 },
       { mq: "768px", columns: 2, gutter: 15 },
-      { mq: "1024px", columns: 3, gutter: 20 },
+      { mq: "1024px", columns: 4, gutter: 10 },
       { mq: "1440px", columns: 4, gutter: 40 },
       { mq: "1500px", columns: 5, gutter: 10 },
       { mq: "1600px", columns: 5, gutter: 30 },
@@ -45,8 +47,8 @@ const DisplayFavourite = ({ notes, isLoading }) => {
 
   return (
     <>
-      <div className='display__notes__container'>
-        <div ref={mainContainerRef} className='display__notes__ref'>
+      <div className="display__notes__container">
+        <div ref={mainContainerRef} className="display__notes__ref">
           {isLoading ? (
             <Loader />
           ) : (
@@ -57,19 +59,29 @@ const DisplayFavourite = ({ notes, isLoading }) => {
                     ref={(el) => (noteElRefs.current[idx] = el)}
                     key={item?._id}
                     style={{ backgroundColor: `${item?.color}` }}
-                    className='display__notes'
+                    className="display__notes"
                   >
+                    {item?.imageData && (
+                      <div className="notes__image">
+                        <img
+                          src={noteImages[item?._id]}
+                          alt="..."
+                          loading="lazy"
+                        />
+                      </div>
+                    )}
                     <div
                       key={item?.id}
                       ref={(el) => (noteElRefs.current[idx] = el)}
+                      className="notes__content__display"
                     >
-                      <div className='display__notes__title'>
-                        <div className='notes__title'>{item?.title}</div>
+                      <div className="display__notes__title">
+                        <div className="notes__title">{item?.title}</div>
                       </div>
 
-                      <div className='display__notes__content__container'>
+                      <div className="display__notes__content__container">
                         <div
-                          className='display__notes__content'
+                          className="display__notes__content"
                           dangerouslySetInnerHTML={{
                             __html: item?.content,
                           }}
@@ -79,7 +91,7 @@ const DisplayFavourite = ({ notes, isLoading }) => {
                   </div>
                 ))
               ) : (
-                <div className='favourite__message'>
+                <div className="favourite__message">
                   Add to view liked Notes
                 </div>
               )}
